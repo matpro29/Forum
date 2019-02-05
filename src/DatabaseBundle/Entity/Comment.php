49c43f9e61f6b809e47abea5e1f71a9cba2dbd88
Mateusz Proskura <matpro29@outlook.com>
2019-02-05 20:30:46 +0100

    Podmiana sztywnych linkow

--- src/DatabaseBundle/Entity/Comment.php
+++ src/DatabaseBundle/Entity/Comment.php
@@ -8,140 +8,82 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity
  * @ORM\Table(name="orm_comment")
  */
-
 class Comment
 {
-
     /**
      * @ORM\Column(type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-
     private $id;
 
     /**
      * @ORM\Column(type="integer")
      */
-
     private $idImage;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-
     private $user;
 
     /**
      * @ORM\Column(type="text")
      */
-
     private $comment;
 
     /**
      * @ORM\Column(type="datetime")
      *
      */
-
     private $date;
 
 
-
-
-
-
-
-    /**
-     * @return mixed
-     */
     public function getId()
     {
         return $this->id;
     }
 
-    /**
-     * @param mixed $id
-     */
     public function setId($id)
     {
         $this->id = $id;
     }
 
-    /**
-     * @return mixed
-     */
     public function getIdImage()
     {
         return $this->idImage;
     }
 
-    /**
-     * @param mixed $idImage
-     */
     public function setIdImage($idImage)
     {
         $this->idImage = $idImage;
     }
 
-    /**
-     * @return mixed
-     */
     public function getUser()
     {
         return $this->user;
     }
 
-    /**
-     * @param mixed $user
-     */
     public function setUser($user)
     {
         $this->user = $user;
     }
 
-    /**
-     * @return mixed
-     */
-    public function getTitle()
-    {
-        return $this->title;
-    }
-
-    /**
-     * @param mixed $title
-     */
-    public function setTitle($title)
-    {
-        $this->title = $title;
-    }
-
-    /**
-     * @return mixed
-     */
     public function getComment()
     {
         return $this->comment;
     }
 
-    /**
-     * @param mixed $comment
-     */
     public function setComment($comment)
     {
         $this->comment = $comment;
     }
 
-    /**
-     * @return mixed
-     */
     public function getDate()
     {
         return $this->date;
     }
 
-    /**
-     * @param mixed $date
-     */
     public function setDate($date)
     {
         $this->date = $date;
